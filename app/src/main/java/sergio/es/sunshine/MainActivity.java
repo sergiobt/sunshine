da68073e9fe559328365e4561cc96ffb57c51290
sergiobt <serbt125@gmail.com>
2017-04-03 12:55:09 +0200

    Arreglado problemas con Menu.

--- app/src/main/java/sergio/es/sunshine/MainActivity.java
+++ app/src/main/java/sergio/es/sunshine/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu); // Error
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) { // Error
+        if (id == R.id.action_settings) {
             return true;
         }
 
