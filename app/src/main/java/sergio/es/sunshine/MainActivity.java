2db48b00aa188efe5a3996e1ceb956a918b96c30
sergiobt <serbt125@gmail.com>
2017-04-23 20:24:14 +0200

    Terminada conexion Open Weather Map
    Creada la conexion con la API Open Weather Map.

--- app/src/main/java/sergio/es/sunshine/MainActivity.java
+++ app/src/main/java/sergio/es/sunshine/MainActivity.java
@@ -1,8 +1,8 @@
 package sergio.es.sunshine;
 
-import android.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,9 +11,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
-                    .commit(); // Error
+                    .commit();
         }
     }
 
@@ -60,7 +61,10 @@ public class MainActivity extends AppCompatActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> forecastAdapter;
+
         public PlaceholderFragment() {
+
         }
 
         @Override
@@ -73,13 +77,13 @@ public class MainActivity extends AppCompatActivity {
             // We put the array into a List to view it.
             List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
 
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
             // getActivity(): The current context (this activity).
             // R.layout.list_item_forecast: the name of the layout ID.
             // R.id.list_item_forecast_textview The ID of the TextView to populate.
             // weekForecast: Forecast data.
-            ArrayAdapter<String> forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+            forecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
             // Get a reference to the ListView and attach this adapter to it.
             ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
@@ -104,16 +108,54 @@ public class MainActivity extends AppCompatActivity {
 
                 // TODO Terminar de crear la conexion.
                 // Create the request to OpenWeatherMap, and open the connection.
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
 
-            }
-            catch (IOException e) {
+                // Read the input  stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null; // Nothing to do.
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a lot easier if you print out the completed buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
                 Log.e("PlaceholderFragment", "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
+            } finally {
 
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
             }
+
             return rootView;
+
         }
     }
 }
